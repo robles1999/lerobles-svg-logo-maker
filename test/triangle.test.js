0f7d131596c1e9823d4d6a6bad2dd500cca81fc3
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-04-21 15:17:04 -0400

    made some minor spacing changes

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -1,18 +1,15 @@
-const Shape = require("../lib/shape");
 const Triangle = require("../lib/triangle");
 
-describe("Shape", () => {
-  describe("Triangle", () => {
-    it("should take three initials, text color and fill color to generate a logo SVG file in a triangle shape", () => {
-      const [text, textColor, shapeColor] = ["CMR", "white", "green"];
-      const triangle = new Triangle(text, textColor, shapeColor);
+describe("Triangle", () => {
+  it("should take three initials, text color and fill color to generate a logo SVG file in a triangle shape", () => {
+    const [text, textColor, shapeColor] = ["ABC", "white", "green"];
+    const triangle = new Triangle(text, textColor, shapeColor);
 
-      expect(triangle.render()).toEqual(`
+    expect(triangle.render()).toEqual(`
     <svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             <rect width="300" height="200" fill="white" stroke="#000000" stroke-width="2" />
             <polygon points="10,190 290,190 150,10" fill="${shapeColor}"/>
             <text fill="${textColor}" font-size="75" text-anchor="middle" x="155" y="160">${text}</text>
     </svg>`);
-    });
   });
 });
