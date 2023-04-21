63028fdda628a2b2e99a33a0e806d730a2abd0d3
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-04-21 09:17:20 -0400

    all tests are working.

--- test/circle.test.js
+++ test/circle.test.js
@@ -3,15 +3,15 @@ const Circle = require("../lib/circle");
 
 describe("Shape", () => {
   describe("Circle", () => {
-    it("should take three initials, text color and fill color to generate a logo SVG file", () => {
-      const [initials, textColor, fillColor] = ["CMR", "white", "green"];
-        const circle = new Circle(initials, textColor, fillColor);
+    it("should take three initials, text color and fill color to generate a logo SVG file in a circle shape", () => {
+      const [text, textColor, shapeColor] = ["CMR", "white", "green"];
+        const circle = new Circle(text, textColor, shapeColor);
         
       expect(circle.render()).toEqual(`
     <svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             <rect width="300" height="200" fill="white" stroke="#000000" stroke-width="2" />
-            <circle cx="150" cy="100" r="90" fill="${fillColor}" />
-            <text fill="${textColor}" font-size="80" text-anchor="middle" x="150" y="120">${initials}</text>
+            <circle cx="150" cy="100" r="90" fill="${shapeColor}" />
+            <text fill="${textColor}" font-size="80" text-anchor="middle" x="150" y="120">${text}</text>
     </svg>`);
     });
   });
---
+++ test/square.test.js
@@ -0,0 +1,18 @@
+const Shape = require("../lib/shape");
+const Square = require("../lib/square");
+
+describe("Shape", () => {
+  describe("Square", () => {
+    it("should take three initials, text color and fill color to generate a logo SVG file in square shape", () => {
+      const [text, textColor, shapeColor] = ["CMR", "white", "green"];
+      const square = new Square(text, textColor, shapeColor);
+
+      expect(square.render()).toEqual(`
+    <svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            <rect width="300" height="200" fill="white" stroke="#000000" stroke-width="2" />
+            <rect x="50" y="50" width="200" height="100" fill="${shapeColor}" />
+            <text fill="${textColor}" font-size="80" text-anchor="middle" x="150" y="120">${text}</text>
+    </svg>`);
+    });
+  });
+});
