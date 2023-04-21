ac42505703f29895a5bd2bc3807260c1624777b2
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-04-21 09:08:24 -0400

    circle test is working.

--- test/circle.test.js
+++ test/circle.test.js
@@ -1,18 +1,18 @@
+const Shape = require("../lib/shape");
 const Circle = require("../lib/circle");
 
-describe("Circle", () => {
+describe("Shape", () => {
   describe("Circle", () => {
     it("should take three initials, text color and fill color to generate a logo SVG file", () => {
-      const [initials, textColor, fillColor] = ["LER", "white", "green"];
-      const circle = new Circle(initials, textColor, fillColor);
-      expect(
-        circle.render().toEqual(`
+      const [initials, textColor, fillColor] = ["CMR", "white", "green"];
+        const circle = new Circle(initials, textColor, fillColor);
+        
+      expect(circle.render()).toEqual(`
     <svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             <rect width="300" height="200" fill="white" stroke="#000000" stroke-width="2" />
             <circle cx="150" cy="100" r="90" fill="${fillColor}" />
-            <text fill="${textColor}" font-size="80" text-anchor="middle" x="150" y="120">${text}</text>
-    </svg>`)
-      );
+            <text fill="${textColor}" font-size="80" text-anchor="middle" x="150" y="120">${initials}</text>
+    </svg>`);
     });
   });
 });
